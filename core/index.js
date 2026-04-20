fa5252e3255078bbbf99d4a123360f35023d10bc
offers-png <offers@dealdily.com>
2026-04-20 19:51:13 -0400

    Add CORS middleware to Express app

--- core/index.js
+++ core/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const app = express();
+app.use(cors());
 app.use(express.json());
 const { startWakeWordListener } = require('./wake-word');
 const { transcribe, recordAudio } = require('./stt');
